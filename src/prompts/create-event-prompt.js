befb2fd202f9aae374daac2a5e39cc354f5dd133
Andrej Staš <andrej.stas.net@gmail.com>
2023-06-06 13:06:44 +0200

    feat: update prompts

--- src/prompts/create-event-prompt.js
+++ src/prompts/create-event-prompt.js
@@ -2,7 +2,7 @@ export const CREATE_EVENT_PROMPT = `
 Date format: YYYY-MM-DDThh:mm:ss+00:00
 Based on this event description: "Joey birthday tomorrow at 7 pm",
 output a json of the following parameters: 
-Today's datetime on UTC time 2021-05-02T10:00:00+00:00, it's Friday and timezone
+Today's datetime on UTC time 2023-05-02T10:00:00+00:00, it's Tuesday and timezone
 of the user is -5, take into account the timezone of the user and today's date.
 1. event_summary 
 2. event_start_time 
@@ -13,17 +13,17 @@ of the user is -5, take into account the timezone of the user and today's date.
 event_summary:
 {{
     "event_summary": "Joey birthday",
-    "event_start_time": "2021-05-03T19:00:00-05:00",
-    "event_end_time": "2021-05-03T20:00:00-05:00",
+    "event_start_time": "2023-05-03T19:00:00-05:00",
+    "event_end_time": "2023-05-03T20:00:00-05:00",
     "event_location": "",
     "event_description": "",
     "user_timezone": "America/New_York"
 }}
 
 Date format: YYYY-MM-DDThh:mm:ss+00:00
-Based on this event description: "Create a meeting for 5 pm on Wednesday with Joey",
+Based on this event description: "Create a meeting for 5 pm on Saturday with Joey",
 output a json of the following parameters: 
-Today's datetime on UTC time 2021-05-02T10:00:00+00:00, it's {dayName} and timezone
+Today's datetime on UTC time 2023-05-04T10:00:00+00:00, it's Thursday and timezone
 of the user is -5, take into account the timezone of the user and today's date.
 1. event_summary 
 2. event_start_time 
@@ -34,8 +34,8 @@ of the user is -5, take into account the timezone of the user and today's date.
 event_summary:
 {{
     "event_summary": "Meeting with Joey",
-    "event_start_time": "2021-05-04T17:00:00-05:00",
-    "event_end_time": "2021-05-04T18:00:00-05:00",
+    "event_start_time": "2023-05-06T17:00:00-05:00",
+    "event_end_time": "2023-05-06T18:00:00-05:00",
     "event_location": "",
     "event_description": "",
     "user_timezone": "America/New_York"
--- src/prompts/view-events-prompt.js
+++ src/prompts/view-events-prompt.js
@@ -1,8 +1,8 @@
 export const VIEW_EVENTS_PROMPT = `
 Date format: YYYY-MM-DDThh:mm:ss+00:00
-Based on this event description: 'View my events on Saturday',
+Based on this event description: 'View my events on Thursday',
 output a json of the following parameters: 
-Today's datetime on UTC time 2023-05-02T10:00:00+00:00, it's Thursday and timezone
+Today's datetime on UTC time 2023-05-02T10:00:00+00:00, it's Tuesday and timezone
 of the user is -5, take into account the timezone of the user and today's date.
 If the user is searching for events with a specific title, person or location, put it into the search_query parameter.
 1. time_min 
